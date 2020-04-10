37dc147f2310a9580508b707e91b22cd69c65caa
Gustavo Zotarelli <gustavozotarelli@gmail.com>
2020-04-10 16:09:03 -0300

    Implementação a classe Estado.

--- src/br/com/fiap/nac/dao/PacienteDAO.java
+++ src/br/com/fiap/nac/dao/PacienteDAO.java
@@ -3,7 +3,7 @@ package br.com.fiap.nac.dao;
 import br.com.fiap.nac.entity.Paciente;
 
 /**
- * Classe responsável por implementar um reositório para {@link Paciente}.
+ * Classe responsável por implementar um repositório para {@link Paciente}.
  *
  * @author Brazil Code - Gabriel Guarido
  * @since 9 de abr de 2020 21:49:56
---
+++ src/br/com/fiap/nac/entity/Estado.java
@@ -0,0 +1,69 @@
+package br.com.fiap.nac.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * Classe responsável por mapear a entidade ESTADO.
+ *
+ * @author Brazil Code - Gustavo Zotarelli
+ * @since 10 de abr de 2020 15:15:10
+ * @version 1.0
+ */
+@Entity
+@Table(name = "ESTADOS")
+@SequenceGenerator(name = "estado", sequenceName = "ESTADO_SEQ", allocationSize = 1)
+public class Estado {
+
+	/**
+	 * Atributo id
+	 */
+	@Id
+	@GeneratedValue(generator = "estado", strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	/**
+	 * Atributo descricao
+	 */
+	@Column(length = 50)
+	private String descricao;
+
+	/**
+	 * Construtor da classe Estado
+	 */
+	public Estado() {
+		super();
+	}
+
+	/**
+	 * Construtor da classe Estado
+	 *
+	 * @param descricao
+	 */
+	public Estado(String descricao) {
+		super();
+		setDescricao(descricao);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
