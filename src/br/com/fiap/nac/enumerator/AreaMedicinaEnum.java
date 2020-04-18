080bd43bcfd656c614d3800a916d1a05ef5d84b0
Gabriel Oliveira <gabriel.oliveira@engdb.com.br>
2020-04-18 18:58:09 -0300

    Implementando Services + MedicoConsoleView

--- src/br/com/fiap/nac/enumerator/AreaMedicinaEnum.java
+++ src/br/com/fiap/nac/enumerator/AreaMedicinaEnum.java
@@ -2,9 +2,27 @@ package br.com.fiap.nac.enumerator;
 
 public enum AreaMedicinaEnum {
 
-	CARDIOLOGIA,
-	CIRURGIA_GERAL,
-	CLINICA,
-	EMERGENCIA;
+	CARDIOLOGIA("Cardiologia"),
+	CIRURGIA_GERAL("Cirurgia Geral"),
+	CLINICA("Clínica"),
+	EMERGENCIA("Emergência");
+
+	/**
+	 * Atributo descricao
+	 */
+	private String descricao;
+
+	/**
+	 * Construtor da classe AreaMedicinaEnum
+	 *
+	 * @param descricao
+	 */
+	private AreaMedicinaEnum(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
 
 }
