922697ba5d2e1fb577ac09eabbb3eae2c9622276
Andrew <andrew.jpb@outlook.com>
2020-04-19 14:49:29 -0300

    Adicionado methods e view Endereço

--- src/br/com/fiap/nac/service/EnderecoService.java
+++ src/br/com/fiap/nac/service/EnderecoService.java
@@ -1,15 +1,19 @@
 package br.com.fiap.nac.service;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.fiap.nac.dao.EnderecoDAO;
 import br.com.fiap.nac.dao.impl.EnderecoDAOImpl;
 import br.com.fiap.nac.entity.Endereco;
+import br.com.fiap.nac.entity.Usuario;
+import br.com.fiap.nac.exception.CommitException;
 import br.com.fiap.nac.exception.ResourceNotFoundException;
 import br.com.fiap.nac.singleton.EntityManagerFactorySingleton;
 
 /**
- * Classe responsável por aplicar as regras de negócio para {@link Endereco}.
+ * Classe responsï¿½vel por aplicar as regras de negï¿½cio para {@link Endereco}.
  *
  * @author Brazil Code - Gabriel Guarido
  * @since 18 de abr de 2020 17:22:42
@@ -28,19 +32,61 @@ public class EnderecoService {
 	private EnderecoDAO enderecoDAO = new EnderecoDAOImpl(EM);
 
 	/**
-	 * Método responsável por buscar um {@link Endereco} no banco de dados de acordo com o ID recebido por parâmetro. Se nenhum
-	 * registro for encontrado uma exceção será lançada.
+	 * Mï¿½todo responsï¿½vel por buscar um {@link Endereco} no banco de dados de acordo com o ID recebido por parï¿½metro. Se nenhum
+	 * registro for encontrado uma exceï¿½ï¿½o serï¿½ lanï¿½ada.
 	 *
 	 * @author Brazil Code - Gabriel Guarido
 	 * @return
 	 * @throws ResourceNotFoundException
 	 */
 	public Endereco findOne(final Long id) throws ResourceNotFoundException {
-		return this.enderecoDAO.findOne(id).orElseThrow(() -> new ResourceNotFoundException("Endereco não encontrado"));
+		return this.enderecoDAO.findOne(id).orElseThrow(() -> new ResourceNotFoundException("Endereco nï¿½o encontrado"));
+	}
+	
+	
+	/**
+	 * Mï¿½todo responsï¿½vel por atualizar as informaï¿½ï¿½es de um {@link Endereco} no banco de dados de acordo com os dados recebidos no
+	 * objeto que estï¿½ sendo passado por parï¿½metro.
+	 *
+	 * @author Brazil Code - Andrew Pereira
+	 * @param usuario
+	 * @throws CommitException
+	 */
+	public void update(final Endereco endereco) throws CommitException {
+		this.enderecoDAO.update(endereco);
+		this.enderecoDAO.commit();
+	}
+	
+	/**
+	 * Mï¿½todo responsï¿½vel por buscar todos os {@link Endereco}'s existentes no banco de dados. Se nenhum registro for encontrado
+	 * uma exceï¿½ï¿½o serï¿½ lanï¿½ada.
+	 *
+	 * @author Brazil Code - Andrew Pereira
+	 * @return
+	 * @throws ResourceNotFoundException
+	 */
+	public List<Endereco> findAll() throws ResourceNotFoundException {
+		return this.enderecoDAO.findAll().orElseThrow(() -> new ResourceNotFoundException("Nenhum endereco cadastrado"));
 	}
 
+	
 	/**
-	 * Método responsável por fechar a instancia do EntityManager.
+	 * Mï¿½todo responsï¿½vel por remover um {@link Endereco} no banco de dados de acordo com o ID recebido por parï¿½metro. Se o
+	 * registro que serï¿½ deletado nï¿½o for encontrado uma exceï¿½ï¿½o serï¿½ lanï¿½ada.
+	 *
+	 * @author Brazil Code - Andrew Pereira
+	 * @param id
+	 * @throws ResourceNotFoundException
+	 * @throws CommitException
+	 */
+	public void delete(final Long id) throws ResourceNotFoundException, CommitException {
+		this.enderecoDAO.delete(id);
+		this.enderecoDAO.commit();
+	}
+
+
+	/**
+	 * Mï¿½todo responsï¿½vel por fechar a instancia do EntityManager.
 	 *
 	 * @author Brazil Code - Gabriel Guarido
 	 */
--- src/br/com/fiap/nac/view/EnderecoConsoleView.java
+++ src/br/com/fiap/nac/view/EnderecoConsoleView.java
@@ -1,5 +1,7 @@
 package br.com.fiap.nac.view;
 
+import java.util.Calendar;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
@@ -8,6 +10,8 @@ import br.com.fiap.nac.dao.impl.CidadeDAOImpl;
 import br.com.fiap.nac.dao.impl.EnderecoDAOImpl;
 import br.com.fiap.nac.entity.Cidade;
 import br.com.fiap.nac.entity.Endereco;
+import br.com.fiap.nac.entity.Usuario;
+import br.com.fiap.nac.service.EnderecoService;
 import br.com.fiap.nac.singleton.EntityManagerFactorySingleton;
 
 /**
@@ -20,7 +24,7 @@ import br.com.fiap.nac.singleton.EntityManagerFactorySingleton;
 public class EnderecoConsoleView {
 
 	/**
-	 * Método responsável por realizar o CRUD da entidade.
+	 * Mï¿½todo responsï¿½vel por realizar o CRUD da entidade.
 	 *
 	 * @author Brazil Code - Andrew Pereira
 	 * @param args
@@ -30,21 +34,55 @@ public class EnderecoConsoleView {
 		EntityManagerFactory factory = EntityManagerFactorySingleton.getInstance();
 		EntityManager em = factory.createEntityManager();
 
+		//Buscando Cidade existente
 		Cidade cidade = new CidadeDAOImpl(em).findOne(1l).get();
 		EnderecoDAO dao = new EnderecoDAOImpl(em);
+		
+		// Criando e cadastrando novo ednereÃ§o
 		Endereco endereco = new Endereco("Capao", "05798100", "Casa2", "Rua gersom marques da silva", cidade);
 
 		// Persist entity
 		try {
 			dao.save(endereco);
 			dao.commit();
+			
+			
+			//instanciando service
+			EnderecoService enderecoService = new EnderecoService();
+
+			
+			// Buscando endereco criado
+			Endereco enderecoBD = enderecoService.findOne(endereco.getId());
+			System.out.println(enderecoBD.toString());
+			
+			// Atualizando a endereco do Usuario criado
+			enderecoBD.setBairro("bairro");
+			enderecoBD.setCep("00000-000");
+			enderecoBD.setComplemento("complemento");
+			enderecoBD.setLogradouro("Logradouro");
+
+			enderecoService.update(enderecoBD);
+			System.out.println(enderecoBD.toString());
+			
+			// Removendo EndereÃ§os criado
+			enderecoService.delete(enderecoBD.getId());
+			
+
+			// Buscando todos os EndereÃ§os existentes
+			enderecoService.findAll().forEach(end -> {
+				System.out.println(end.toString());
+			});
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		
 		// Closing factory n entity manager
 		em.close();
 		factory.close();
 	}
 
 }
+
+
